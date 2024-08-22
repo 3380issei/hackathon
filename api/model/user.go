c09e787c2a826a54b97e17f2d257e2ff40c6fec7
3380issei <3380issei@gmail.com>
2024-08-22 14:09:57 +0900

    User modelにunique制約を追加

--- api/model/user.go
+++ api/model/user.go
@@ -3,6 +3,6 @@ package model
 type User struct {
 	ID       int    `json:"id" gorm:"primary_key"`
 	Name     string `json:"name"`
-	Email    string `json:"email"`
+	Email    string `json:"email" gorm:"unique"`
 	Password string `json:"password"`
 }
