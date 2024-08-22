6c51dfdd2d230d7c070ed1700169aadc8aa7452b
3380issei <3380issei@gmail.com>
2024-08-22 13:56:34 +0900

    UserとScheduleのリレーションを定義

--- api/model/schedule.go
+++ api/model/schedule.go
@@ -4,8 +4,9 @@ import "time"
 
 type Schedule struct {
 	ID        int       `json:"id" gorm:"primary_key"`
-	UserID    int       `json:"user_id"`
 	Latitude  float64   `json:"latitude"`
 	Longitude float64   `json:"longitude"`
 	Deadline  time.Time `json:"deadline"`
+	User      User      `json:"user" gorm:"foreignkey:UserID"`
+	UserID    int       `json:"user_id"`
 }
