0acc7215f58f0f7f56f13d6df4676cbd6ee9f158
3380issei <3380issei@gmail.com>
2024-08-24 16:23:54 +0900

    Scheduleに目的地の名前を表すカラムDestinationを追加

--- api/model/schedule.go
+++ api/model/schedule.go
@@ -3,13 +3,14 @@ package model
 import "time"
 
 type Schedule struct {
-	ID        int       `json:"id" gorm:"primary_key"`
-	Latitude  float64   `json:"latitude"`
-	Longitude float64   `json:"longitude"`
-	Deadline  time.Time `json:"deadline"`
-	Expired   bool      `json:"expired" default:"false"`
-	User      User      `json:"user" gorm:"foreignkey:UserID"`
-	UserID    int       `json:"user_id" gorm:"not null"`
+	ID          int       `json:"id" gorm:"primary_key"`
+	Destination string    `json:"destination"`
+	Latitude    float64   `json:"latitude"`
+	Longitude   float64   `json:"longitude"`
+	Deadline    time.Time `json:"deadline"`
+	Expired     bool      `json:"expired" default:"false"`
+	User        User      `json:"user" gorm:"foreignkey:UserID"`
+	UserID      int       `json:"user_id" gorm:"not null"`
 }
 
 type CurrentLocation struct {
