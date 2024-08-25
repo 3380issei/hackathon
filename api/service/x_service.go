7a05dbc041ea1494ea480a17556e19d8d9a048b7
dasukaru <dasukaru46@gmail.com>
2024-08-25 13:32:33 +0900

    文言の微調整

--- api/service/x_service.go
+++ api/service/x_service.go
@@ -25,12 +25,18 @@ type XServiceInterface interface {
 	Post(schedule model.Schedule) error
 }
 
+// 時間表示のstring化と成型
+func time_to_str(t time.Time) string {
+	return t.Format("2006年1月2日 15:04")
+}
+
 func (xs *XService) Post(user model.User, schedule model.Schedule) error {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	//環境変数回りを理解，実行環境にenvファイルが置け次第即変更すること
 	in := &gotwi.NewClientInput{
 		AuthenticationMethod: gotwi.AuthenMethodOAuth1UserContext,
 		OAuthToken:           "1820304527171743744-EhQgkpLo00G5O4TqwXBVJ0Bh3q7CUn",
@@ -45,7 +51,7 @@ func (xs *XService) Post(user model.User, schedule model.Schedule) error {
 
 	var post_content string
 
-	post_content += user.Name + "さんは目的地に予定通り到着することができませんでした．\n"
+	post_content += user.Name + "さんは目的地に宣言通り到着できませんでした．\n"
 	post_content += "到着予定場所は " + schedule.Destination + "(" + fmt.Sprint(schedule.Latitude) + ", " + fmt.Sprint(schedule.Longitude) + ")\n"
 	post_content += "期限は " + time_to_str(schedule.Deadline) + " まででした．\n"
 	post_content += "自分で設定しておいて何故時間通りにたどり着けないのです？"
@@ -60,8 +66,4 @@ func (xs *XService) Post(user model.User, schedule model.Schedule) error {
 		return err
 	}
 	return nil
-}
-
-func time_to_str(t time.Time) string {
-	return t.Format("2006年1月2日 15:04")
 }
